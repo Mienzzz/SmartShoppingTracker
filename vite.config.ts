561290cf7c943878d24ac7e802ac6e602c81e641
Mienzzz <icemintblack@gmail.com>
2026-01-24 10:12:00 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_');
-
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
-    define: {
-      __GEMINI_API_KEY__: JSON.stringify(env.VITE_GEMINI_API_KEY),
-    },
-  };
+  },
 });
